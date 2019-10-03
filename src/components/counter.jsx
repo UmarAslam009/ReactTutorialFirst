7abe95710048eee1a54a1880111be7f1d8803381
umar aslam <umar@trimulabs.com>
2019-10-03 23:07:40 +1100

    updateted project

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,25 +1,19 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-
-  state = {
-    value: this.props.value,
-    tags: ["tag1", "tag2", "tag3"]
-  };
-
-  increment = () => {
-    this.setState({ value: this.state.value + 1 });
-  };
   render() {
     let classes = this.applyClass();
     return (
       <div>
         <span className={classes}>{this.formatCount()}</span>
-        <button onClick={this.increment} className="btn btn-primary btn-sm">
+        <button
+          onClick={() => this.props.onIncrement(this.props.counter)}
+          className="btn btn-primary btn-sm"
+        >
           Increment
         </button>
         <button
-          onClick={() => this.props.onDelete(this.props.id)}
+          onClick={() => this.props.onDelete(this.props.counter.id)}
           className="btn btn-danger btn-sm m-2"
         >
           Delete
@@ -28,12 +22,12 @@ class Counter extends Component {
     );
   }
   formatCount() {
-    const count = this.state.value;
+    const count = this.props.counter.value;
     return count === 0 ? "zero" : count;
   }
   applyClass() {
-    let classes = "badge m-2";
-    classes += this.state.value === 0 ? " badge-warning" : "badge-primary";
+    let classes = " m-2 badge badge-warning ";
+    classes += this.props.counter === 0 ? " badge-warning" : "badge-primary";
     return classes;
   }
 }
